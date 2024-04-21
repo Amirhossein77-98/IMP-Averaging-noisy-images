e27c26d1d54cf1e64c6012776cc4758cf12b6cd8
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2024-04-21 11:19:40 +0330

    Implemented basic starter code

--- mian.py
+++ mian.py
@@ -3,3 +3,19 @@ import numpy as np
 import os
 import csv
 
+# Reading the image
+image_path = "original.jpg"
+image = cv2.imread(image_path)
+
+def add_salt_and_pepper_noise(image, amount):
+    pass
+
+# Create directories to save noisy samples and results
+os.makedirs("noisy_samples", exist_ok=True)
+os.makedirs("result", exist_ok=True)
+
+# Generate noisy samples
+samples = 500
+for i in range(samples):
+    noise_amount = np.random.uniform(0.01, 0.05)
+    noisy_samples = add_salt_and_pepper_noise(image, amount=noise_amount)
