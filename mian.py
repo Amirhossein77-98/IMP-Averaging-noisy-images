b51a3c3c52547a4d5050a49fa59f9734badc5ce1
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2024-04-22 11:49:34 +0330

    Fixed the pdf generation bugs
    Of course I couldn't find a good solution to fix it dynamically for all image sizes. But it works fine now.

--- mian.py
+++ mian.py
@@ -75,9 +75,10 @@ def calculate_average_and_variance(image):
 
 def generate_pdf_report(samples):
     from reportlab.lib.pagesizes import landscape, A5
-    from reportlab.platypus import SimpleDocTemplate, Image, Paragraph
+    from reportlab.platypus import SimpleDocTemplate, Image, Paragraph, Spacer
     from reportlab.lib.styles import getSampleStyleSheet
     from reportlab.lib.units import inch
+    from reportlab.platypus import Table, TableStyle
 
     print("Generating pdf report...")
 
@@ -87,25 +88,26 @@ def generate_pdf_report(samples):
     subtitle_style = styles["Heading3"]
 
     # Set up the PDF document
-    doc = SimpleDocTemplate("result/results.pdf", pagesize=landscape(A5))
+    doc = SimpleDocTemplate("result/results.pdf", pagesize=landscape(A5),topMargin=0*inch, bottomMargin=0*inch)
     elements = []
 
     # Add the original image with title
-    original_image = Image("original.jpg", width=3*inch, height=2*inch)
+    original_image = Image("original.jpg", width=3.5*inch, height=4*inch)
     elements.append(Paragraph("Original Image", title_style))
     elements.append(original_image)
+    # Add a spacer for bottom margin
+    elements.append(Spacer(0, 1*inch))
 
     # Add the denoised images with titles
     image_files = [f"result/result_for_{i}_samples.jpg" for i in samples]
     titles = ["k=1", "k=5", "k=10", "k=20", "k=50", "k=100"]
 
-    from reportlab.platypus import Table, TableStyle
     table_data = []
     for i in range(0, len(image_files), 3):
         row = []
         for j in range(3):
             if i + j < len(image_files):
-                image = Image(image_files[i + j], width=2*inch, height=1.5*inch)
+                image = Image(image_files[i + j], width=1.5*inch, height=2*inch)
                 title = Paragraph(titles[i + j], subtitle_style)
                 row.append([image, title])
         table_data.append(row)
@@ -113,7 +115,9 @@ def generate_pdf_report(samples):
     table = Table(table_data, colWidths=[2.5*inch, 2.5*inch, 2.5*inch])
     table.setStyle(TableStyle([('VALIGN', (0, 0), (-1, -1), 'MIDDLE')]))
 
+    elements.append(Spacer(0, 0.3*inch))
     elements.append(Paragraph("Denoised Images", title_style))
+    elements.append(Spacer(0, 0.3*inch))
     elements.append(table)
 
     # Build the PDF document
@@ -133,7 +137,7 @@ def main():
     samples = 500
     print(f"Generating {samples} noisy images...")
     for i in range(samples):
-        noise_amount = np.random.uniform(0.01, 0.05)
+        noise_amount = np.random.uniform(0.01, 0.1)
         noisy_samples = add_salt_and_pepper_noise(image, amount=noise_amount)
         cv2.imwrite(f"noisy_samples/noisy_sample_{i}.jpg", noisy_samples)
 
